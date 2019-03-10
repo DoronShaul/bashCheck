d16ea2caa25255c178230403598f399f30adb27b
Doron <doronsds@gmail.com>
2019-03-10 02:07:59 +0200

    documentation added.

--- basicCheck.sh
+++ basicCheck.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 
-CURRDIR=$1
-PROGRAM=$2
+CURRDIR=$1            #the given directory.
+PROGRAM=$2            #the given program.
 MEMCHECK=0
 THREADRACE=0
 COMPILATION=0
 
-if [ ! -f $CURRDIR/Makefile ]; then MEMCHECK=1
+if [ ! -f $CURRDIR/Makefile ]; then MEMCHECK=1      #if there is no makefile in this folder, return 1 in everything (all failed).
 	THREADRACE=1
 	COMPILATION=1
 fi
 
-if [ $COMPILATION == 0 ]; then 
+if [ $COMPILATION == 0 ]; then        #if there is a makefile in this folder, run it.
 	make -f Makefile
 
-	if [ $? != 0 ]; then COMPILATION=1
+	if [ $? != 0 ]; then COMPILATION=1      #if the compilation failed.
 	fi
 
-	if [ $COMPILATION == 0 ]; then
+	if [ $COMPILATION == 0 ]; then          #if the compilation succeed, checks for memory leaked.
 		valgrind --error-exitcode=1 --leak-check=full ./$PROGRAM
 
-		if [ $? != 0 ]; then MEMCHECK=1
+		if [ $? != 0 ]; then MEMCHECK=1       #if there is a memory leaked.
 		fi
 
-		valgrind --error-exitcode=1 --tool=helgrind ./$PROGRAM
+		valgrind --error-exitcode=1 --tool=helgrind ./$PROGRAM       #run thread race test.
 
-		if [ $? != 0 ]; then THREADRACE=1
+		if [ $? != 0 ]; then THREADRACE=1     #if there is a thread race problem.
 		fi
 	fi
 fi
