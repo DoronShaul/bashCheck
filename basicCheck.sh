c1eb07ab0782fd0bd4650c13ba30ab6038987a36
Doron <doronsds@gmail.com>
2019-03-11 12:32:27 +0200

    added exit modes.

--- basicCheck.sh
+++ basicCheck.sh
@@ -6,27 +6,25 @@ MEMCHECK=0
 THREADRACE=0
 COMPILATION=0
 
-if [ ! -f $CURRDIR/Makefile ]; then MEMCHECK=1      #if there is no makefile in this folder, return 1 in everything (all failed).
-	THREADRACE=1
-	COMPILATION=1
+cd $1
+
+make
+
+if [ $? != 0 ]; then       #if the compilation failed or there is no makefile. 
+COMPILATION=1
+MEMCHECK=1
+THREADRACE=1
 fi
 
-if [ $COMPILATION == 0 ]; then        #if there is a makefile in this folder, run it.
-	make -f Makefile
+if [ $COMPILATION == 0 ]; then          #if the compilation succeed, checks for memory leaked.
+	valgrind --error-exitcode=1 --leak-check=full ./$PROGRAM
 
-	if [ $? != 0 ]; then COMPILATION=1      #if the compilation failed.
+	if [ $? != 0 ]; then MEMCHECK=1       #if there is a memory leaked.
 	fi
 
-	if [ $COMPILATION == 0 ]; then          #if the compilation succeed, checks for memory leaked.
-		valgrind --error-exitcode=1 --leak-check=full ./$PROGRAM
+	valgrind --error-exitcode=1 --tool=helgrind ./$PROGRAM       #run thread race test.
 
-		if [ $? != 0 ]; then MEMCHECK=1       #if there is a memory leaked.
-		fi
-
-		valgrind --error-exitcode=1 --tool=helgrind ./$PROGRAM       #run thread race test.
-
-		if [ $? != 0 ]; then THREADRACE=1     #if there is a thread race problem.
-		fi
+	if [ $? != 0 ]; then THREADRACE=1     #if there is a thread race problem.
 	fi
 fi
 
@@ -36,4 +34,22 @@ echo ""
 
 echo "COMPILATION       MEMORY LEAKS       THREAD RACE"
 echo "    $COMPILATION                  $MEMCHECK                  $THREADRACE"     
+
+
+if [ $COMPILATION -eq 1 ]; then exit 7
+fi
+
+if [ $COMPILATION -eq 0 ]; then 
+	if [ $MEMCHECK -eq 1 ]; then
+		if [ $THREADRACE -eq 1 ]; then exit 3
+		else exit 2
+		fi
+	else
+		if [ $THREADRACE -eq 1 ]; then exit 1
+		else exit 0
+		fi
+	fi
+fi
+
+
 
